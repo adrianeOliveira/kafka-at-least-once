1d039bb610750aaecb5efd3e6e9453371070999a
Adriane Rodrigues <rodrigues.adriane@outlook.com>
2022-07-24 20:36:00 -0300

    First commit

---
+++ src/test/java/br/com/adriane/kafka/atleastonce/AtLeastOnceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.adriane.kafka.atleastonce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtLeastOnceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
